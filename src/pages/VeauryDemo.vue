0c8826a6c6299bfaf2f53de9ff6912d47e052683
Mukesh Prajapati <mukesh.prajapati@kombee.com>
2025-06-12 18:16:35 +0530

    Add veaury dependency and update routing for Veaury demo page; clean up MainLayout.vue formatting

---
+++ src/pages/VeauryDemo.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex flex-center">
+    <h2>Vue + React via Veaury</h2>
+    <ReactInVue :msg="'Hello from Quasar SSR!'" />
+  </q-page>
+</template>
+
+<script setup>
+import HelloReact from 'src/react/HelloReact.jsx';
+import * as veaury from 'veaury';
+console.log('veaury:', veaury);
+const { applyReactInVue } = veaury.default || veaury;
+const ReactInVue = applyReactInVue(HelloReact);
+</script>
