c15bffd8152ccf0a59a47dce8b7e39c8910885bd
Mukesh Prajapati <mukesh.prajapati@kombee.com>
2025-06-13 12:12:29 +0530

    Update Veaury demo to use Counter component instead of HelloReact for improved functionality

--- src/pages/VeauryDemo.vue
+++ src/pages/VeauryDemo.vue
@@ -1,14 +1,14 @@
 <template>
   <q-page class="flex flex-center">
     <h2>Vue + React via Veaury</h2>
-    <ReactInVue :msg="'Hello from Quasar SSR!'" />
+    <ReactInVue />
   </q-page>
 </template>
 
 <script setup>
-import HelloReact from 'src/react/HelloReact.jsx';
+import Counter from 'src/react/Counter.tsx';
 import * as veaury from 'veaury';
 console.log('veaury:', veaury);
 const { applyReactInVue } = veaury.default || veaury;
-const ReactInVue = applyReactInVue(HelloReact);
+const ReactInVue = applyReactInVue(Counter);
 </script>
